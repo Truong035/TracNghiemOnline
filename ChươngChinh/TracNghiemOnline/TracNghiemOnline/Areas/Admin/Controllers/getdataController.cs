7add2b997f21ac9215c583f7843877532b1eefec
ADMIN <haupham404@gmail.com>
2021-05-05 23:14:33 +0700

    them

--- ChươngChinh/TracNghiemOnline/TracNghiemOnline/Areas/Admin/Controllers/getdataController.cs
+++ ChươngChinh/TracNghiemOnline/TracNghiemOnline/Areas/Admin/Controllers/getdataController.cs
@@ -147,8 +147,11 @@ namespace TracNghiemOnline.Areas.Admin.Controllers
             return Content("lay  thanh cong");
 
         }
+        
+       
         public ActionResult getstudent()
         {
+            
             var options = new ChromeOptions();
             options.AddArgument("no-sandbox");
             // Chạy ngầm không pop up trình duyệt ra ngoài 
@@ -160,94 +163,215 @@ namespace TracNghiemOnline.Areas.Admin.Controllers
             radiobutton.Click();
 
             var khoa = webDriver.FindElement(By.Name("DropDownList2"));
-            var select = new SelectElement(khoa);
-            select.SelectByValue("558");
-            var lop = webDriver.FindElement(By.Name("DropDownList3"));
-            var selectlop = new SelectElement(lop);
-            selectlop.SelectByValue("5441");
-            var button = webDriver.FindElement(By.XPath("//*[@id='Tim']"));
-            button.Click();
-
-
-
-            var table = webDriver.FindElement(By.XPath("//*[@id='listsinhvien']/div/table/tbody"));
-            var rows_table = table.FindElements(By.TagName("tr"));
-            int rows_count = rows_table.Count;
-            int z = 1;
-            foreach (var item in rows_table)
-            {
-                if (z == 1 || z == rows_count)
+            var selectkhoa = new SelectElement(khoa);
+           
+            var yy = webDriver.FindElements(By.XPath("//*[@id='DropDownList2']/option"));
+            foreach (var j in yy) { 
+                if( j.Text.Equals("Tất cả") ) continue;
+                else if (j.Text.Equals("All")) continue;
+                else if(j.Text.Equals("Khóa 59"))
                 {
+                    selectkhoa.SelectByText(j.Text);
+                    var lop = webDriver.FindElement(By.Name("DropDownList3"));
+                    var selectlop = new SelectElement(lop);
+                    var xx = webDriver.FindElements(By.XPath("//*[@id='DropDownList3']/option"));
 
-                }
+                    foreach (var i in xx)
+            {
+                TracNghiemOnlineDB db = new TracNghiemOnlineDB();
+                if (i.Text.Equals("Tất cả")) continue;
                 else
                 {
-                    var tablesv = item.FindElements(By.TagName("td"));
-                    TracNghiemOnlineDB db = new TracNghiemOnlineDB();
-                   
-                        SinhVien sv = new SinhVien();
-                        sv.MaSV = tablesv[2].Text;
-                        sv.Ten = tablesv[3].Text;
-                    
-                    sv.NgaySinh = DateTime.ParseExact(tablesv[4].Text,"d/M/yyyy", CultureInfo.InvariantCulture);
-                    sv.DiaChi = tablesv[5].Text;
-                    if (db.SinhViens.Where(x => x.MaSV.Equals(sv.MaSV)).ToList().Count <= 0)
+
+                    if (db.Lops.Where(x => x.TenLop.Equals(i.Text)).ToList().Count <= 0)
                     {
-                        db.SinhViens.Add(sv);
-                        db.SaveChanges();
-                    }
-                    
-                   
-                    /*  Console.OutputEncoding = Encoding.Unicode; Console.InputEncoding = Encoding.Unicode;
-                      Console.WriteLine("heloooooooooooo" + tablesv[3].Text);*/
+                                try
+                                {
 
 
-                }
-                z++;
-            }
+                                    Lop lop1 = new Lop();
 
+                                    var malop="";
+                                    int z = 0;
+                                    int vtt = 0;
+                                    var s  = i.Text.ToString().Substring(0, i.Text.ToString().Length);
+                                    if (i.Text.ToString().Contains("Quận"))
+                                    {
+                                        s= i.Text.ToString().Substring(0, i.Text.IndexOf("Quận")-1 );
+                                    }
+                                   
+                                    foreach (var c in s)
+                                    {
+                                        if (z == 0)
+                                        {
+                                            malop = malop + c.ToString();
+                                            
 
-
-
-            for (int slpage = 1; slpage <= 2; slpage++)
-            {
-                var page = webDriver.FindElement(By.XPath("//*[@id='listsinhvien']/table[1]/tbody/tr/td/font/font/b/a[" + slpage + "]"));
-                page.Click();
-
-                var table1 = webDriver.FindElement(By.XPath("//*[@id='listsinhvien']/div/table/tbody"));
-                var rows_table1 = table1.FindElements(By.TagName("tr"));
-                int rows_count1 = rows_table1.Count;
-                int z1 = 1;
-                foreach (var item in rows_table1)
-                {
-                    if (z1 == 1 || z1 == rows_count1)
-                    {
-
-                    }
-                    else
-                    {
-                        var tablesv = item.FindElements(By.TagName("td"));
-                        TracNghiemOnlineDB db = new TracNghiemOnlineDB();
-                        
-                        SinhVien sv = new SinhVien();
-                        sv.MaSV = tablesv[2].Text;
-                        sv.Ten = tablesv[3].Text;
-                        sv.NgaySinh = DateTime.ParseExact(tablesv[4].Text, "d/M/yyyy", CultureInfo.InvariantCulture);
-                        sv.DiaChi = tablesv[5].Text;
-                        if (db.SinhViens.Where(x => x.MaSV.Equals(sv.MaSV)).ToList().Count <= 0)
-                        {
-                            db.SinhViens.Add(sv);
+                                        }
+                                        else if( c.ToString() == " " )
+                                        {
+                                            vtt = z;
+                                            malop = malop + s[z+1].ToString();
+                                        }
+                                       z++;
+                                    }
+                                    if (i.Text.ToString().Contains("Quận"))
+                                    {
+                                        lop1.Ma_Lop = (malop.Substring(0, malop.Length - 1) + i.Text.ToString().Substring(vtt + 4, i.Text.Length - vtt - 4)).ToUpper();
+                                    }
+                                    else 
+                                    lop1.Ma_Lop = ( malop.Substring(0,malop.Length-1) + i.Text.ToString().Substring( vtt, i.Text.Length-vtt)).ToUpper();
+                                    
+                            lop1.TenLop = i.Text.ToString();
+                            lop1.Ma_Nganh = 1;
+                            db.Lops.Add(lop1);
                             db.SaveChanges();
+                            }
+                        catch (Exception e)
+                            {
+
+                            }
+
+
+
                         }
 
-                        /*
-                                                Console.OutputEncoding = Encoding.Unicode; Console.InputEncoding = Encoding.Unicode;
-                                                Console.WriteLine("heloooooooooooo" + tablesv[3].Text);*/
+                    if (i.Text.Equals("Công nghệ thông tin K59"))
+                    {
 
+                        
+                        selectlop.SelectByText(i.Text);
+
+                        var button = webDriver.FindElement(By.XPath("//*[@id='Tim']"));
+                        button.Click();
+                        var table = webDriver.FindElement(By.XPath("//*[@id='listsinhvien']/div/table/tbody"));
+                        var rows_table = table.FindElements(By.TagName("tr"));
+                        int rows_count = rows_table.Count;
+                        int z = 1;
+                        foreach (var item in rows_table)
+                        {
+                            if (z == 1 || z == rows_count)
+                            {
+
+                            }
+                            else
+                            {
+                                var tablesv = item.FindElements(By.TagName("td"));
+
+
+                                SinhVien sv = new SinhVien();
+                                sv.MaSV = tablesv[2].Text;
+                                sv.Ten = tablesv[3].Text;
+
+                                sv.NgaySinh = DateTime.ParseExact(tablesv[4].Text, "d/M/yyyy", CultureInfo.InvariantCulture);
+                                sv.DiaChi = tablesv[5].Text;
+
+                                var tenlop = item.FindElement(By.XPath("//*[@id='lbADMINCLASSID']")).GetAttribute("value");
+                                var malop = db.Lops.Where(x => x.TenLop.Equals(tenlop) && x.DaXoa == null).ToList();
+                                if (malop.Count > 0)
+                                {
+                                    sv.Ma_Lop = malop[0].Ma_Lop.ToString();
+                                }
+
+                                if (db.SinhViens.Where(x => x.MaSV.Equals(sv.MaSV)).ToList().Count <= 0 && tenlop.Length > 0)
+                                {
+
+                                    db.SinhViens.Add(sv);
+                                    db.SaveChanges();
+                                    if (db.TaiKhoans.Where(x => x.TenDangNhap.Equals(sv.MaSV) && x.TenDangNhap.Equals(sv.MaSV)).ToList().Count <= 0)
+                                    {
+                                        TaiKhoan tk = new TaiKhoan();
+                                        tk.TenDangNhap = sv.MaSV;
+                                        tk.TaiKhoan1 = sv.MaSV;
+                                        tk.MatKhau = "1";
+                                        tk.TrangThai = true;
+                                        tk.ChưcVu = "SinhViên";
+                                        db.TaiKhoans.Add(tk);
+                                        db.SaveChanges();
+                                    }
+                                }
+
+
+
+
+                            }
+                            z++;
+                        }
+
+                        var sltrang = webDriver.FindElement(By.XPath("//*[@id='listsinhvien']/table[2]/tbody/tr/td/font[4]")).Text;
+
+
+                        for (int slpage = 1; slpage <= Convert.ToInt32(sltrang)-1; slpage++)
+                        {
+                            var page = webDriver.FindElement(By.XPath("//*[@id='listsinhvien']/table[1]/tbody/tr/td/font/font/b/a[" + slpage + "]"));
+                            page.Click();
+
+                            var table1 = webDriver.FindElement(By.XPath("//*[@id='listsinhvien']/div/table/tbody"));
+                            var rows_table1 = table1.FindElements(By.TagName("tr"));
+                            int rows_count1 = rows_table1.Count;
+                            int z1 = 1;
+                            foreach (var item in rows_table1)
+                            {
+                                if (z1 == 1 || z1 == rows_count1)
+                                {
+
+                                }
+                                else
+                                {
+
+
+
+                                    var tablesv = item.FindElements(By.TagName("td"));
+
+
+                                    SinhVien sv = new SinhVien();
+                                    sv.MaSV = tablesv[2].Text;
+                                    sv.Ten = tablesv[3].Text;
+
+                                    sv.NgaySinh = DateTime.ParseExact(tablesv[4].Text, "d/M/yyyy", CultureInfo.InvariantCulture);
+                                    sv.DiaChi = tablesv[5].Text;
+
+                                    var tenlop = item.FindElement(By.XPath("//*[@id='lbADMINCLASSID']")).GetAttribute("value");
+                                    var malop = db.Lops.Where(x => x.TenLop.Equals(tenlop) && x.DaXoa == null).ToList();
+                                    if (malop.Count > 0)
+                                    {
+                                        sv.Ma_Lop = malop[0].Ma_Lop.ToString();
+                                    }
+
+                                    if (db.SinhViens.Where(x => x.MaSV.Equals(sv.MaSV)).ToList().Count <= 0 && tenlop.Length > 0)
+                                    {
+
+                                        db.SinhViens.Add(sv);
+                                        db.SaveChanges();
+                                        if (db.TaiKhoans.Where(x => x.TenDangNhap.Equals(sv.MaSV) && x.TenDangNhap.Equals(sv.MaSV)).ToList().Count <= 0)
+                                        {
+                                            TaiKhoan tk = new TaiKhoan();
+                                            tk.TenDangNhap = sv.MaSV;
+                                            tk.TaiKhoan1 = sv.MaSV;
+                                            tk.MatKhau = "1";
+                                            tk.TrangThai = true;
+                                            tk.ChưcVu = "SinhViên";
+                                            db.TaiKhoans.Add(tk);
+                                            db.SaveChanges();
+                                        }
+                                    }
+
+
+
+                                }
+                                z1++;
+                            }
+                        }
 
                     }
-                    z1++;
                 }
+               
+
+
+            }
+
+                }
+                
             }
             return Content("lay  thanh cong");
         }
